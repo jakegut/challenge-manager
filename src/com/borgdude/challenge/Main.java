84ed0b3d890f09ffb5d30cc05a1e73ff1f7f5a93
Jake Gutierrez <theborgdude@gmail.com>
2018-06-18 21:33:09 -0500

    Bug Fixes Fix NullPointerException when on first load of plugin. Freaking out when trying to load the challenges if there were none there. Update plugin.yml (name -> Challenge_Manager)

--- src/com/borgdude/challenge/Main.java
+++ src/com/borgdude/challenge/Main.java
@@ -12,6 +12,7 @@ import com.borgdude.challenge.managers.FreezeManager;
 import com.borgdude.challenge.objects.Challenge;
 import com.borgdude.challenge.objects.ChallengeSet;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -29,32 +30,15 @@ public class Main extends JavaPlugin {
         plugin = this;
         freezeManager = new FreezeManager();
         challengeBookManager = new ChallengeBookManager();
-        ArrayList<ChallengeSet> challengeSets = new ArrayList<>();
-        for(String csID : getConfig().getConfigurationSection("challenges").getKeys(false)){
-            UUID uuid = UUID.fromString(csID);
-            String title = getConfig().getString("challenges." + uuid + ".title");
-            ArrayList<Challenge> challenges = new ArrayList<>();
-            for(String chID : getConfig().getConfigurationSection("challenges." + csID).getKeys(false)){
-                try{
-                    UUID chUUID = UUID.fromString(chID);
-                    String desc = getConfig().getString("challenges." + csID + "." + chID + ".description");
-                    int index = getConfig().getInt("challenges." + csID + "." + chID + ".index");
-                    challenges.add(index - 1, new Challenge(desc, chUUID, uuid));
-                } catch (IllegalArgumentException exception){
-                    continue;
-                }
-            }
-            challengeSets.add(new ChallengeSet(title, challenges, uuid));
-        }
-        challengeManager = new ChallengeManager(challengeSets);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        loadChallengeSets();
         getCommand("freeze").setExecutor(new FreezeCommand());
         getCommand("unfreeze").setExecutor(new UnfreezeCommand());
         getCommand("challenge").setExecutor(new ChallengeCommand());
         getCommand("challenge").setTabCompleter(new OnTabComplete());
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Challenges has been enabled");
         getServer().getPluginManager().registerEvents(new EventClass(), this);
-        getConfig().options().copyDefaults(true);
-        saveConfig();
     }
 
     @Override
@@ -72,5 +56,29 @@ public class Main extends JavaPlugin {
         }
         saveConfig();
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "Challenges has been disabled");
+    }
+
+    public void loadChallengeSets(){
+        ArrayList<ChallengeSet> challengeSets = new ArrayList<>();
+        ConfigurationSection section = getConfig().getConfigurationSection("challenges");
+        if(section != null){
+            for(String csID : section.getKeys(false)){
+                UUID uuid = UUID.fromString(csID);
+                String title = getConfig().getString("challenges." + uuid + ".title");
+                ArrayList<Challenge> challenges = new ArrayList<>();
+                for(String chID : getConfig().getConfigurationSection("challenges." + csID).getKeys(false)){
+                    try{
+                        UUID chUUID = UUID.fromString(chID);
+                        String desc = getConfig().getString("challenges." + csID + "." + chID + ".description");
+                        int index = getConfig().getInt("challenges." + csID + "." + chID + ".index");
+                        challenges.add(index - 1, new Challenge(desc, chUUID, uuid));
+                    } catch (IllegalArgumentException exception){
+                        continue;
+                    }
+                }
+                challengeSets.add(new ChallengeSet(title, challenges, uuid));
+            }
+        }
+        challengeManager = new ChallengeManager(challengeSets);
     }
 }
