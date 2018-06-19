7a1df16c6a0bb9a15f89be368e78bfb6cbde6c6c
Jake Gutierrez <theborgdude@gmail.com>
2018-06-18 21:37:15 -0500

    Challenge Book will not be given when an op joins. It will be given when a user completes a challenge "Optimized Imports"

--- src/com/borgdude/challenge/events/EventClass.java
+++ src/com/borgdude/challenge/events/EventClass.java
@@ -4,28 +4,17 @@ import com.borgdude.challenge.Main;
 import com.borgdude.challenge.managers.ChallengeBookManager;
 import com.borgdude.challenge.managers.ChallengeManager;
 import com.borgdude.challenge.managers.FreezeManager;
-import com.borgdude.challenge.objects.Challenge;
 import com.borgdude.challenge.objects.ChallengeSet;
 import com.borgdude.challenge.util.SetBookUtil;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.chat.ComponentSerializer;
-import net.minecraft.server.v1_12_R1.IChatBaseComponent;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftMetaBook;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class EventClass implements Listener {
 
@@ -63,14 +52,6 @@ public class EventClass implements Listener {
 
         SetBookUtil.updateSetBook(player, this.challengeManager.getAssignedChallengeSets().get(player).getChallenges().get(0));
 
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event){
-        Player player = (Player) event.getPlayer();
-        if(player.hasPermission("challenges.admin")){
-            this.challengeBookManager.addBook(player);
-        }
     }
 
     @EventHandler
